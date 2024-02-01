44b3bcc430365eaf00e5bfe147f6418f2a208f1c
Harukaah <140624361+Harukaah@users.noreply.github.com>
2024-02-01 19:59:00 +0900

    Update lab15_2.cpp

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -2,12 +2,22 @@
 #include <algorithm>
 using namespace std;
 
-int main(){
-	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
-	cout << "Before: ";
-	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	sort(&data[0],&data[10]);
-	cout << "\nAfter: ";
-	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	return 0;
+void customSort(int* start, int* end) {
+    sort(start, end);
 }
+
+int main() {
+    int data[] = {7, 8, 2, 1, 2, 4, 6, 9, 0, 1};
+    cout << "Before: ";
+    for (int i = 0; i < 10; i++) cout << data[i] << " ";
+
+    int* mid = &data[5];
+    customSort(&data[0], mid);
+    customSort(mid, &data[10]);
+
+    cout << "\nAfter: ";
+    for (int i = 0; i < 10; i++) cout << data[i] << " ";
+
+    return 0;
+}
+
